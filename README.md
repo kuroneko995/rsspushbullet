a4fe4a13cea03d65cfc1bc09dc0b9b7be67ebaef
Dang Minh Nguyen <ndminh92@gmail.com>
2015-08-30 04:36:37 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+rsspushbullet
+============
+
+Python script that filter rss feeds and send notification to pushbullet 
+I wrote this to notify myself for new anime release on animetosho
+
+For python 2
+
+**Dependencies**
+
+This depends on pyPushBullet and feedparser. 
+You can follow the instruction to install them here:
+https://github.com/Azelphur/pyPushBullet
+https://pypi.python.org/pypi/feedparser
+
+You will need to provide your pushbullet api key.
+You can get your API Key from https://www.pushbullet.com/#settings/account
+
+Using the script:
+
+Clone the git directory and run
+python parse_feeds.py
---
+++ parse_feeds.py
@@ -0,0 +1,95 @@
+# Require https://github.com/Azelphur/pyPushBullet
+import feedparser
+from pushbullet.pushbullet import PushBullet
+apiKey = ""
+
+def read_config():
+  feeds_file = open("feeds.txt",'r')
+  feeds = []
+  current_feed = {}
+
+  for line in feeds_file:
+    if (line[0] == '#') or (line.isspace()):
+      continue
+    elif line[0] == '+':
+      feeds[-1]["positive"].append(line[1:-1])
+    elif line[0] == '-':
+      feeds[-1]["negative"].append(line[1:-1])
+    elif line.split()[0] == "pushbullet_api":
+      global apiKey
+      apiKey = line.split()[2]
+    else:
+      current_feed = dict()
+      current_feed["url"] = line[:-1]
+      current_feed["positive"] = []
+      current_feed["negative"] = []
+      feeds.append(current_feed)
+         
+  feeds_file.close()
+  return feeds
+
+def read_log():
+  log_file = open("log.txt",'r')
+  previous_files = []
+  for line in log_file:
+    previous_files.append(line[:-1])
+  log_file.close()
+  return previous_files
+
+def write_log(entries_list):
+  log_file = open("log.txt",'a')
+  for entry in entries_list:
+    log_file.write(entry["title"] + '\n')
+  log_file.close()
+
+def push_link(entries_list):
+  print apiKey
+  client = PushBullet(apiKey)
+  devices = client.getDevices()
+  chrome = []
+  for device in devices:
+    if device["type"] == "chrome":
+      chrome = device
+      break
+  identity = chrome["iden"]
+
+  for entry in entries_list:
+    client.pushLink(identity, entry["title"], entry["link"])
+
+def main():
+  feeds = read_config()
+  previous_titles = read_log()
+  new_entries = []
+  for feed_info in feeds:
+    print "Parsing:", feed_info
+    entries = filter_feed(feed_info)
+    for entry in entries:
+      if entry["title"] not in previous_titles:
+        new_entries.append(entry)
+        print "New file found", entry["title"]
+
+  push_link(new_entries)
+  write_log(new_entries)
+
+def filter_feed(feed_info):
+  new_entries = []
+  feed = feedparser.parse(feed_info["url"])
+  for entry in feed["entries"]:
+    is_valid = True
+    title = entry["title"]
+    for keyword in feed_info["positive"]:
+      if keyword not in title:
+        is_valid = False
+        break
+    if not is_valid:
+      continue
+    for keyword in feed_info["negative"]:
+      if keyword in title:
+        is_valid = False
+        break
+    if is_valid:
+      new_entries.append(entry)
+
+  return new_entries
+    
+main()
